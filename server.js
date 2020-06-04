038594c0b79a0d227e1cd9d65ab0a9915aba87a5
Adrian Zhang <adrian.zhang@shopee.com>
2020-06-04 15:43:56 +0800

    save

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`<html maaa=a >
+  res.end(`<html>
   <head>
     <style>
       #container{
